a76ddbdeb86de2989866b51d45383937417cd929
Madeline Joubert <madelinejoubert44@gmail.com>
2020-01-21 22:30:33 +0100

    adding wateringDate

--- back/install.sql
+++ back/install.sql
@@ -2,6 +2,7 @@ CREATE TABLE plant (
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   name VARCHAR(255),
   spray INT,
+  wateringDate DATETIME,
   description VARCHAR(255),
   picture VARCHAR(255)
 );
